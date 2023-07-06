26b66195a5a8f3464a2001a711552691b11c3a5e
Codigo Uranio <codigouranio@gmail.com>
2023-07-05 23:55:23 -0400

    added cron task
    Signed-off-by: Codigo Uranio <codigouranio@gmail.com>

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const LinceScheduler = class LinceScheduler {
     const defaults = {
       startIntervalMs: 1000,
       intervalMs: 1000,
-      maxIntervalMs: 1000 * 60 * 60,
+      maxIntervalMs: 1000 * 60 * 60, 
       maxRetries: 3,
       handler: new Handler(),
       parser: new Parser(),
@@ -40,6 +40,46 @@ const LinceScheduler = class LinceScheduler {
   }
 
   /**
+   * Schedule a message fetcher with a cron expression.
+   * @param {*} cronExpression 
+   * @param {*} messageFetcher 
+   * @returns 
+   */
+  schedule(options) {
+    const task = new ScheduleTask(options);
+    task.start(() => {
+      console.log(`test ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
+    });
+    return task;
+    // return new Promise((resolve, reject) => {
+    //   const defaults = {
+    //     cronExpression: '* * * * *',
+    //     messageFetcher: new MessageFetcher(),
+    //     maxLoops: -1
+    //   }
+    //   options = Object.assign({}, defaults, options);
+    //   const cronTask = new CronTask(options.cronExpression);
+
+    // });
+  }
+
+  // async scheduleCronTask(options, resolve, reject) {
+  //   setTimeout(async () => {
+  //     try {
+  //       const messages = await options.messageFetcher.fetchMessages();
+  //       if (Array.isArray(messages)) {
+  //         this.logger.info({}, `Found ${messages.length} messages`);
+  //         await Promise.allSettled(messages.map((message) => this.execute(message)));
+  //       }
+  //     } catch (error) {
+  //       this.logger.error({}, error);
+  //       return reject(error);
+  //     }
+  //     return resolve();
+  //   }, cronTask.calculateInterval());
+  // }
+
+  /**
    * Execute a job.
    * @param {*} originalMessage
    * @returns
@@ -47,9 +87,8 @@ const LinceScheduler = class LinceScheduler {
   async execute(originalMessage) {
     const job = new Job({
       maxRetries: this.maxRetries,
-      originalMessage
+      originalMessage,
     });
-
     try {
       this.logger.info(job, 'Parsing message');
       job.setMessage(this.parser.parse(this, originalMessage));
@@ -60,7 +99,7 @@ const LinceScheduler = class LinceScheduler {
     }
     job.setLocalStorage(this.localStorage);
     try {
-      await this.schedulePromise(job);
+      await this.retryPromise(job, this.calculateInterval(job));
     } catch (error) {
       this.logger.error(job, error);
     }
@@ -72,18 +111,17 @@ const LinceScheduler = class LinceScheduler {
    * @param {*} job
    * @returns
    */
-  async schedulePromise(job) {
+  async retryPromise(job, interval) {
     this.stats.totalPending++;
     return new Promise((resolve, reject) => {
-      this.localStorage.run(job, () => this.schedule(job, resolve, reject));
+      this.localStorage.run(job, () =>
+        this.retry(job, interval, resolve, reject)
+      );
     });
   }
 
-  async schedule(job, resolve, reject) {
-    setTimeout(
-      () => this.handleJob(job, resolve, reject),
-      this.calculateInterval(job)
-    );
+  retry(job, interval, resolve, reject) {
+    setTimeout(() => this.handleJob(job, resolve, reject), interval);
   }
 
   handleJob(job, resolve, reject) {
@@ -111,7 +149,7 @@ const LinceScheduler = class LinceScheduler {
 
       this.logger.error(job, error);
       job.setLastError(error);
-      this.schedule(job, resolve, reject);
+      this.retry(job, this.calculateInterval(job), resolve, reject);
     }
   }
 
@@ -138,11 +176,169 @@ const LinceScheduler = class LinceScheduler {
   }
 };
 
+class ScheduleTask {
+  constructor(options) {
+    const defaults = {
+      promise: new Promise((resolve, reject) => {
+        this.resolve = resolve;
+        this.reject = reject;
+      }),
+      maxLoops: 3,
+      stopped: false, 
+      cronTask: new CronTask(options.cronExpression),
+    };
+
+    options = Object.assign({}, defaults, options);
+
+    this.promise = options.promise;
+    this.maxLoops = options.maxLoops;
+    this.cronTask = options.cronTask;
+    this.started = false;
+    this.stopped = false;
+  }
+
+  /**
+   * Start the task.
+   * @param {*} funcTask 
+   */
+  start(funcTask) {
+    this.timeoutId = setTimeout(() => {
+      funcTask();
+      if (this.maxLoops > 0) {
+        this.maxLoops--;
+        this.start(funcTask);
+      }
+      if (this.maxLoops == 0) {
+        this.stop();
+      }
+    }, this.cronTask.calculateInterval());
+  }
+
+  stop() {
+    clearTimeout(this.timeoutId);
+    this.resolve();
+  }
+
+  setInterval(interval) {
+    this.interval = interval;
+  }
+
+  setResolve(resolve) {
+    this.resolve = resolve;
+  }
+
+  getResolve() {
+    return this.resolve;
+  }
+
+  setReject(reject) {
+    this.reject = reject;
+  }
+
+  getReject(reject) {
+    return this.reject;
+  }
+
+  getMaxLoops() {
+    return this.maxLoops;
+  }
+
+  setIntervalId(intervalId) {
+    this.intervalId = intervalId;
+  }
+
+  getIntervalId() {
+    return this.intervalId;
+  }
+
+  getPromise() {
+    return this.promise;
+  }
+}
+
 class Handler {
   constructor(options) {}
 
   handle(job) {
     return { message: 'not implemented' };
+  }
+}
+
+class MessageFetcher {
+  constructor(options) {}
+
+  async fetchMessages(options) {
+    console.log('fetchMessages');
+    return [{}, {}];
+  }
+}
+
+class CronTask {
+  constructor(cronExpression) {
+    this.cronExpression = cronExpression;
+
+    Object.defineProperty(this, 'cronExpression', {
+      name: 'cronExpression',
+      enumerable: false,
+    });
+  }
+
+  calculateInterval() {
+    const cronParts = this.cronExpression.split(' ');
+    const [minute, hour, dayOfMonth, month, dayOfWeek] = cronParts;
+    const now = moment();
+    let nextExecution = now.clone();
+
+    if (minute.startsWith('*/')) {
+      const seconds = this.getSecondsfromCronExpression(minute);
+      nextExecution = nextExecution.add(seconds, 'second');
+    }
+    if (minute !== '*') {
+      nextExecution.set('minute', minute);
+      if (now.minute() > nextExecution.minute()) {
+        nextExecution = nextExecution.add(1, 'hour');
+      }
+    }
+    if (hour !== '*') {
+      nextExecution.set('hour', hour);
+      if (now.hour() > nextExecution.hour()) {
+        nextExecution = nextExecution.add(1, 'day');
+      }
+    }
+    if (dayOfMonth !== '*') {
+      nextExecution.set('date', dayOfMonth);
+      if (now.date() > dayOfMonth) {
+        nextExecution = nextExecution.add(1, 'month');
+      }
+    }
+    if (month !== '*') {
+      nextExecution.set('month', month - 1);
+      if (now.month() > month - 1) {
+        nextExecution = nextExecution.add(1, 'year');
+      }
+    }
+    if (dayOfWeek !== '*') {
+      nextExecution.set('day', dayOfWeek);
+      if (now.day() < dayOfWeek) {
+        nextExecution = nextExecution.add(1, 'week');
+      }
+    }
+    var duration = moment.duration(nextExecution.diff(now));
+    return duration.asMilliseconds();
+  }
+
+  getSecondsfromCronExpression(expression) {
+    const regex = /\/(\d+)/g;
+    const matches = [];
+    let match;
+    while ((match = regex.exec(expression)) !== null) {
+      const number = parseInt(match[1]);
+      matches.push(number);
+    }
+    if (matches.length > 0) {
+      return matches[0];
+    }
+    return 0;
   }
 }
 
@@ -368,7 +564,7 @@ const ConsoleLogger = class ConsoleLogger {
   }
 
   isJobClass(job) {
-    return (job instanceof Job);
+    return job instanceof Job;
   }
 };
 
@@ -377,5 +573,7 @@ module.exports = {
   Job,
   ConsoleLogger,
   Handler,
-  ParsingError
+  ParsingError,
+  CronTask,
+  MessageFetcher,
 };
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
-const { LinceScheduler, ParsingError } = require('./index');
+const { LinceScheduler, ParsingError, CronTask } = require('./index');
+const moment = require('moment');
 
 describe('Test Execution', () => {
 
@@ -19,11 +20,9 @@ describe('Test Execution', () => {
     });
     expect(scheduler).toBeInstanceOf(LinceScheduler);
   
-    const job = await scheduler.execute('test');
-    expect(job.wasSuccessful()).toBeTruthy();
+    const job = await scheduler.execute('test');    expect(job.wasSuccessful()).toBeTruthy();
   });
 
-  
   test('test error execution', async () => {
     const handler = {
       handle(job) {
@@ -60,6 +59,90 @@ describe('Test Execution', () => {
     expect(job.wasError()).toBeTruthy();
     expect(job.getLastError()).toBeInstanceOf(ParsingError);
     expect(job.getLastError().message).toBe('test error parsing');
+  });
+
+  test('test cron task forward', async () => {
+    const cronTaskMinute = new CronTask({
+      cronExpression: `${moment().add(1, 'minute').minute()} * * * * *`,
+    });
+    expect(cronTaskMinute.calculateInterval()).toBeGreaterThanOrEqual(60000);
+
+    const cronTaskHour = new CronTask({
+      cronExpression: `* ${moment().add(1, 'hour').hour()} * * * *`,
+    });
+    expect(cronTaskHour.calculateInterval()).toBeGreaterThanOrEqual(360000);
+
+    const cronTaskDay = new CronTask({
+      cronExpression: `* * ${moment().add(1, 'day').date()} * * *`,
+    });
+    expect(cronTaskDay.calculateInterval()).toBeGreaterThanOrEqual(86400000);
+
+    const cronTaskMonth = new CronTask({
+      cronExpression: `* * * ${moment().add(1, 'month').month()} *`
+    });
+    expect(cronTaskMonth.calculateInterval()).toBeGreaterThanOrEqual(2678400000);
+
+    const tomorrow = moment().add(1, 'day');
+    const cronTaskDayOfWeek = new CronTask({
+      cronExpression: `* * * * ${tomorrow.day()}`
+    });
+    expect(cronTaskDayOfWeek.calculateInterval()).toBeGreaterThanOrEqual(86400000);
+  });
+
+  test('test cron task backward', async () => {
+    const cronTaskMinute = new CronTask({
+      cronExpression: `${moment().subtract(1, 'minute').minute()} * * * * *`,
+    });
+    expect(cronTaskMinute.calculateInterval()).toBe(3540000);
+
+    const cronTaskHour = new CronTask({
+      cronExpression: `* ${moment().subtract(1, 'hour').hour()} * * * *`,
+    });
+    expect(cronTaskHour.calculateInterval()).toBe(82800000);
+
+    const cronTaskDay = new CronTask({
+      cronExpression: `* * ${moment().subtract(1, 'day').date()} * * *`,
+    });
+    expect(cronTaskDay.calculateInterval()).toBe(2592000000);
+
+    const cronTaskMonth = new CronTask({
+      cronExpression: `* * * ${moment().subtract(1, 'month').month() + 1} *`
+    });
+    expect(cronTaskMonth.calculateInterval()).toBe(29030400000);
+    console.log(cronTaskMonth.calculateInterval());
+
+    const tomorrow = moment().add(1, 'day');
+    const cronTaskDayOfWeek = new CronTask({
+      cronExpression: `* * * * ${tomorrow.day()}`
+    });
+    expect(cronTaskDayOfWeek.calculateInterval()).toBeGreaterThanOrEqual(86400000);
+  });
+
+  test('test cron task backward', async () => {
+    const cronTaskTens = new CronTask({
+      cronExpression: `*/10 * * * *`,
+    });
+
+    expect(cronTaskTens.calculateInterval()).toBe(10000);
+  });
+
+  test.only('test schedule fetcher', async () => {
+    const handler = {
+      handle(job) {
+        throw new Error('test');
+      }
+    };
+    const scheduler = new LinceScheduler({
+      handler
+    });
+    expect(scheduler).toBeInstanceOf(LinceScheduler);
+
+    const task = scheduler.schedule({
+      cronExpression: '*/10 * * * *'
+    });
+    await task.getPromise();
+
+    console.log('finished');
   });
 
 });
